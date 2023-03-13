218958c5f2f0f5f2cab62fa53751f575ff753d5c
Radha Sharma <radhasharma2786@gmail.com>
2023-03-13 22:49:02 +0530

    day1

---
+++ Fundamentals/String.js
@@ -0,0 +1,15 @@
+let str = "Radha Sharma";
+console.log(str[0]); // to access character at that index
+console.log(str[99]); // undefined
+console.log(str.toUpperCase()); //  Destructive method (do not change original string)
+
+let s = "     Radha and Harshita      ";
+console.log(s.trim()); // to remove spaces from the starting and end (Destructive --will not change original string)
+console.log(s);
+// function chaining
+console.log(s.trim().toUpperCase());
+let vr = str.substring(2);
+let vr2 = str.substring(2, 5);
+// till 5 it will give
+
+console.log(vr);
