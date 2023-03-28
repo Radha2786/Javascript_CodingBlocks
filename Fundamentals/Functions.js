873e4ace7276bd5bc96eb6acd7e05579c8ef6c91
Radha Sharma <radhasharma2786@gmail.com>
2023-03-28 23:04:09 +0530

    changes are done

---
+++ Fundamentals/Functions.js
@@ -0,0 +1,21 @@
+// ----------Javascript Functions---------------
+
+function fun() {
+  console.log("heyyyaa");
+}
+
+fun();
+  // STRONGLYY TYPED VS WEEKLY TYPED LANGUAGES
+
+//   Javascript is a weekely typed language : ex- 
+//   let a = 10;
+//   a="Radha"
+//   this can be done (means we can change the datatype)
+// whereas C/C++/Java are strongly types
+// let a = 10;
+//   a="Radha"
+// this is not allowed here (means we can not convert the data type)
+
+
+// STATICALLY VS DYNAMICALLY TYPED LANGUAGES
+// JS IS dynamically typed language that is during compile Time we don't know the datatype of variable
---
+++ Fundamentals/Objects.js
@@ -0,0 +1,18 @@
+// JS OBJECTS  (objects are also reference type)
+
+// ---> Collection of properties(key-value pairs)
+// ---> it is a data structure
+// --->Unsorted
+
+const obj = {
+    firstName:'Radha',
+    secondName: 'Sharma',
+    age:19,
+    isAdult: true
+}
+
+// console.log(obj);
+// console.log(obj.firstName);
+console.log(obj['first'+'Name']);
+// console.log(obj["firstName"]); 
+// we are giving in the form of string cuz behind the scene the keys gets converted into string
