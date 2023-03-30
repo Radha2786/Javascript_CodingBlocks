e8323748f7c836961273c5aeb66509c5a3013b64
Radha Sharma <radhasharma2786@gmail.com>
2023-03-31 00:01:15 +0530

    lecture 12

--- Fundamentals/Objects.js
+++ Fundamentals/Objects.js
@@ -4,15 +4,39 @@
 // ---> it is a data structure
 // --->Unsorted
 
-const obj = {
-    firstName:'Radha',
-    secondName: 'Sharma',
-    age:19,
-    isAdult: true
-}
+// const obj = {
+//     firstName:'Radha',
+//     secondName: 'Sharma',
+//     age:19,
+//     isAdult: true
+// }
 
 // console.log(obj);
 // console.log(obj.firstName);
-console.log(obj['first'+'Name']);
-// console.log(obj["firstName"]); 
+// console.log(obj['first'+'Name']);
+// console.log(obj["firstName"]);
 // we are giving in the form of string cuz behind the scene the keys gets converted into string
+
+// console.log(""==0) ;  // empty string get typecaste to 0
+// op = true
+
+// console.log(""===0);
+// op = false;
+
+// similarly empty array gets typecaste to string and string gets typecaste to 0
+// console.log([]=="");  // true
+// console.log([]==0); // true
+
+// const f = function fun() {
+//   console.log("Inside fun");
+// };
+// console.log(f);
+// f();
+
+// Anonymous function as function expression
+let x =50;
+const f = function () {
+  console.log("Inside fun");
+};
+console.log(x);
+f();
